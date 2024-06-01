c070988baf7f467078a37bc3b1ed9f6f5f2faf3b
Niteshwebdev <worldbest178@gmail.com>
2024-05-31 23:31:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+require('dotenv').config(); // Load environment variables
 
 const connectDB = require("./database/db");
 const { Signupapi } = require("./Api/signupapi");
@@ -16,50 +17,22 @@ const { Loginapi } = require("./Api/loginapi");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Secret key generation
-// const secretKey = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-// if (!process.env.SESSION_SECRET) {
-//   process.env.SESSION_SECRET = secretKey;
-// }
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-    ],
+    origin: "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
-app.use(express.static(path.join(__dirname,'./frontend/build')))
-
-app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, './frontend/build/index.html'))
-})
-
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: false }, // Set 'secure: true' if using HTTPS
-// }));
-
-
-
-// app.use(express.static(path.join(__dirname, './frontend/build')))
-
-// app.get("*", function(req,res){
-//     res.sendFile(path.join(__dirname, './frontend/build/index.html'))
-// })
-
-
-
+// Serve static files
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use('/images/product', express.static(path.join(__dirname, 'images/product')));
 
+// API routes
 app.post("/signupapi", profilepicupload.single('profilePic'), Signupapi);
 app.post("/addproductapi", addproductupload.single('pimage'), Addproductapi);
 app.get("/showproductapi", Showproductapi);
@@ -69,8 +42,23 @@ app.post("/loginapi", Loginapi);
 // app.post("/logoutapi", Logoutapi);
 // app.post("/session", Session);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
-connectDB();
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the server after DB connection is established
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(error => {
+  console.error("Failed to connect to the database. Server not started.");
+  process.exit(1);
+});
