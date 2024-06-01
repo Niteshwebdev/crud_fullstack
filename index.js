037ff4390c15b536b593993cdbc30f7454a60481
Niteshwebdev <worldbest178@gmail.com>
2024-05-31 22:39:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -50,11 +50,11 @@ app.get('*', function(req,res){
 
 
 
-app.use(express.static(path.join(__dirname, './frontend/build')))
+// app.use(express.static(path.join(__dirname, './frontend/build')))
 
-app.get("*", function(req,res){
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
-})
+// app.get("*", function(req,res){
+//     res.sendFile(path.join(__dirname, './frontend/build/index.html'))
+// })
 
 
 
