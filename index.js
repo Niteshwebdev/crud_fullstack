775c0dc5c71a2a1033b40e762e17e8d6c181b330
Niteshwebdev <worldbest178@gmail.com>
2024-07-09 12:19:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://frontend-stack-3ld7.onrender.com/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
