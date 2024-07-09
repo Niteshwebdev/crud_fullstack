627cd24ab7ac55a6918a7c5f7a33dcf33eee0605
Niteshwebdev <worldbest178@gmail.com>
2024-07-09 12:23:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://frontend-stack-3ld7.onrender.com/",
+    origin: "https://crud-fullstack-3q1g.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
